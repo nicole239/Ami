2d7854a9bb7f0e9e24948785801a057ed5397a49
nicole239 <nicole.carvajal.b@gmail.com>
2019-10-30 11:31:32 -0600

    Perfil fragment

--- app/src/main/java/ec/tec/ami/views/activities/MainActivity.java
+++ app/src/main/java/ec/tec/ami/views/activities/MainActivity.java
@@ -35,18 +35,25 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
-
+        setupIcons(tabLayout);
 
     }
 
     private void setupViewPager(ViewPager viewPager){
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new TimeLineFragment(),"TimeLine");
-        adapter.addFragment(new PerfilFragment(),"Perfil");
-        adapter.addFragment(new AmigosFragment(),"Amigos");
-        adapter.addFragment(new BusquedaFragment(),"Busqueda");
-        adapter.addFragment(new NotificationsFragment(),"Notificaciones");
+        adapter.addFragment(new TimeLineFragment(),"");
+        adapter.addFragment(new PerfilFragment(),"");
+        adapter.addFragment(new AmigosFragment(),"");
+        adapter.addFragment(new BusquedaFragment(),"");
+        adapter.addFragment(new NotificationsFragment(),"");
         viewPager.setAdapter(adapter);
+    }
+
+    private void setupIcons(TabLayout tabLayout){
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_access_time_black_24dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_account_circle_black_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_group_black_24dp);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_search_black_24dp);
+        tabLayout.getTabAt(4).setIcon(R.drawable.ic_notifications_black_24dp);
     }
 }
