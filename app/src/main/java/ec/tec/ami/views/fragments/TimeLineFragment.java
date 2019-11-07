e1ae02939716bd3896b8be4d10df38a4eeb2aad1
Kahho <kahho.chen@gmail.com>
2019-11-07 13:32:51 -0600

    Added Post List & Youtube video player

--- app/src/main/java/ec/tec/ami/views/fragments/TimeLineFragment.java
+++ app/src/main/java/ec/tec/ami/views/fragments/TimeLineFragment.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,6 +23,7 @@ import ec.tec.ami.R;
 import ec.tec.ami.data.dao.PostCursor;
 import ec.tec.ami.model.Post;
 import ec.tec.ami.views.activities.PostActivity;
+import ec.tec.ami.views.adapters.PostAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +32,8 @@ public class TimeLineFragment extends Fragment {
 
     private EditText txtNewPost;
     private View view;
+    private RecyclerView listaPosts;
+    private LinearLayoutManager mLayoutManager;
 
     public TimeLineFragment() {
         // Required empty public constructor
@@ -37,6 +43,9 @@ public class TimeLineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_time_line, container, false);
+        listaPosts = view.findViewById(R.id.timeline_recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        listaPosts.setLayoutManager(mLayoutManager);
         txtNewPost = view.findViewById(R.id.txtNewPost);
         txtNewPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +69,11 @@ public class TimeLineFragment extends Fragment {
             @Override
             public void onDataFetched(List<Post> posts) {
                 System.out.println("aaaa");
+                Toast.makeText(getContext(),"Posts",Toast.LENGTH_LONG).show();
+
+                PostAdapter adapter = new PostAdapter(getContext(),posts);
+                listaPosts.setAdapter(adapter);
+
             }
 
             @Override
