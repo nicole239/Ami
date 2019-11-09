b9eae843fcf1110faa32176891242747cc07e156
nicole239 <nicole.carvajal.b@gmail.com>
2019-11-08 19:46:32 -0600

    View basic profile data

--- app/src/main/java/ec/tec/ami/views/fragments/PerfilFragment.java
+++ app/src/main/java/ec/tec/ami/views/fragments/PerfilFragment.java
@@ -1,6 +1,7 @@
 package ec.tec.ami.views.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -8,21 +9,32 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import ec.tec.ami.R;
+import ec.tec.ami.data.dao.UserDAO;
+import ec.tec.ami.data.event.UserEvent;
 import ec.tec.ami.model.Education;
 import ec.tec.ami.model.User;
+import ec.tec.ami.views.activities.CreateAccountActivity;
+import ec.tec.ami.views.activities.LoginActivity;
 import ec.tec.ami.views.adapters.PostAdapter;
 
 
@@ -30,15 +42,14 @@ public class PerfilFragment extends Fragment {
 
     RelativeLayout layoutPerfilInfo;
     private boolean showingInfo = false;
-    TextView txtName;
-    TextView txtEmail;
-    TextView txtBirthday;
-    TextView txtGender;
-    TextView txtCity;
-    TextView txtPhone;
+    TextView txtName, txtEmail, txtBirthday, txtGender, txtCity, txtPhone;
     ListView listEducation;
     RecyclerView listPosts;
     PostAdapter postAdapter;
+    ImageView imgUser;
+
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
 
     public PerfilFragment() {
         // Required empty public constructor
@@ -59,6 +70,7 @@ public class PerfilFragment extends Fragment {
         txtPhone = view.findViewById(R.id.txtPerfilPhone);
         listEducation  = view.findViewById(R.id.listPerfilEducation);
         listPosts = view.findViewById(R.id.listPerfilPosts);
+        imgUser = view.findViewById(R.id.imgPerfilPicture);
 
         Button btnPerfilViewData = view.findViewById(R.id.btnPerfilViewData);
         btnPerfilViewData.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +79,9 @@ public class PerfilFragment extends Fragment {
                 btnPerfilViewData_onClick();
             }
         });
+        Log.i("PERFIL_TAG","Iniciando busqueda de usuario...");
+        setCurrentUser();
+
 
         return view;
     }
@@ -81,9 +96,12 @@ public class PerfilFragment extends Fragment {
     }
 
     private void setData(User user){
+        if(user.getProfilePhoto()!= null && !user.getProfilePhoto().isEmpty()) {
+            Glide.with(getContext()).load(user.getProfilePhoto()).into(imgUser);
+        }
         txtName.setText(user.getName() +" "+ user.getLastNameA() +" "+ user.getLastNameB());
         txtEmail.setText(user.getEmail());
-        txtBirthday.setText( user.getBirthDay().toString());
+        txtBirthday.setText( df.format(user.getBirthDay()));
         txtGender.setText(user.getGender());
         txtCity.setText(user.getCity());
         txtPhone.setText(String.valueOf(user.getTelephone()));
@@ -100,6 +118,22 @@ public class PerfilFragment extends Fragment {
         listPosts.setAdapter(postAdapter);
     }
 
+    private void setCurrentUser(){
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        Log.i("PERFIL_TAG",email);
+        UserDAO.getInstance().getUser(email, new UserEvent() {
+            @Override
+            public void onSuccess(User user){
+                Log.i("PERFIL_TAG",user.getName());
+                setData(user);
+
+            }
+            @Override
+            public  void onFailure(Exception e){
+                Log.i("PERFIL_TAG",e.getMessage());
+            }
+        });
+    }
 
 
 }
